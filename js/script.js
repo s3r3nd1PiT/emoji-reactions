78eeee00ceba1726936eeb58133fdf28fa1b9619
Sεяεηδιριτ <s3r3nd1p1t@gmail.com>
2023-09-30 23:33:34 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -50,5 +50,54 @@ reactionButtons.forEach(button => {
     });
 });
 
+function emojisplosion(button, emoji) {
+    const explosionCount = 10;
+    const container = document.createElement('div');
+    container.style.position = 'absolute';
+    container.style.top = `${button.offsetTop}px`;
+    container.style.left = `${button.offsetLeft}px`;
+    document.body.appendChild(container);
+
+    for (let i = 0; i < explosionCount; i++) {
+        const span = document.createElement('span');
+        span.innerText = emoji;
+        span.style.position = 'absolute';
+        span.style.fontSize = '20px';
+        span.style.transition = 'all 0.5s ease-out';
+        span.style.transform = `translate(${Math.random() * 100 - 50}px, ${Math.random() * 100 - 50}px)`;
+        span.style.opacity = 0;
+        container.appendChild(span);
+    }
+
+    setTimeout(() => {
+        document.body.removeChild(container);
+    }, 500);
+}
+
+// Modify the button click event listener to add the explosion effect
+reactionButtons.forEach(button => {
+    button.addEventListener('click', async (e) => {
+        const emoji = e.target.dataset.emoji;
+        const container = e.target.closest('.emoji-reaction-container');
+        const id = container.dataset.id;
+
+        const response = await fetch('/.netlify/functions/add-reaction', {
+            method: 'POST',
+            body: JSON.stringify({ emoji, id }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const data = await response.json();
+        if (data && data.count) {
+            button.innerText = `${emoji} ${data.count}`;
+            emojisplosion(button, emoji);
+        } else {
+            console.error('Unexpected response:', data);
+        }
+    });
+});
+
+
 // Load counts when the page loads
 loadCounts();

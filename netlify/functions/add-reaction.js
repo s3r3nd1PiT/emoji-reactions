5b65c5efa94c1cedab9a087c6fec4fb870ece5e2
Sεяεηδιριτ <s3r3nd1p1t@gmail.com>
2023-09-30 23:14:26 +0200

    Update add-reaction.js

--- netlify/functions/add-reaction.js
+++ netlify/functions/add-reaction.js
@@ -1,6 +1,11 @@
-let reactions = {};
-
 exports.handler = async (event, context) => {
+    if (event.httpMethod === 'GET') {
+        return {
+            statusCode: 200,
+            body: JSON.stringify(reactions)
+        };
+    }
+
     if (event.httpMethod !== 'POST') {
         return { statusCode: 405, body: 'Method Not Allowed' };
     }

3d51e204ae4e0ebab46e7719d322f973b8dca861
Sεяεηδιριτ <s3r3nd1p1t@gmail.com>
2023-10-01 12:51:48 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,37 +1,37 @@
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDD9U4hnfQnzEduCfjTqhJE8xHN98iDY2s",
+    authDomain: "emoji-reactions-1e77e.firebaseapp.com",
+    databaseURL: "https://emoji-reactions-1e77e-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "emoji-reactions-1e77e",
+    storageBucket: "emoji-reactions-1e77e.appspot.com",
+    messagingSenderId: "73302211585",
+    appId: "1:73302211585:web:cbb224bfa5e332ea809400"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+
 const reactionButtons = document.querySelectorAll('.emoji-reaction');
 
-// Fetch current counts and animate them
-async function loadCounts() {
-    const response = await fetch('/.netlify/functions/add-reaction');
-    const data = await response.json();
-
-    document.querySelectorAll('.emoji-reaction-container').forEach(container => {
-        const id = container.dataset.id;
-        if (data[id]) {
-            container.querySelectorAll('.emoji-reaction').forEach(button => {
-                const emoji = button.dataset.emoji;
-                const countSpan = button.querySelector('.emoji-count');
-                if (data[id][emoji]) {
-                    animateCount(button, data[id][emoji]);
-                }
-            });
-        }
+// Fetch current counts from Firebase and display them
+function loadCounts() {
+    database.ref('reactions/').once('value').then((snapshot) => {
+        const data = snapshot.val();
+        document.querySelectorAll('.emoji-reaction-container').forEach(container => {
+            const id = container.dataset.id;
+            if (data && data[id]) {
+                container.querySelectorAll('.emoji-reaction').forEach(button => {
+                    const emoji = button.dataset.emoji;
+                    const countSpan = button.querySelector('.emoji-count');
+                    if (data[id][emoji]) {
+                        countSpan.innerText = data[id][emoji];
+                    }
+                });
+            }
+        });
     });
-}
-
-// Animate count from 0 to actual value in 3 seconds
-function animateCount(button, targetCount) {
-    let currentCount = 0;
-    const increment = targetCount / 30; // 30 frames for 3 seconds
-    const countSpan = button.querySelector('.emoji-count');
-    const interval = setInterval(() => {
-        currentCount += increment;
-        if (currentCount >= targetCount) {
-            clearInterval(interval);
-            currentCount = targetCount;
-        }
-        countSpan.innerText = Math.round(currentCount);
-    }, 100); // 100ms interval for smooth animation
 }
 
 // Emojisplosion effect
@@ -45,7 +45,7 @@ function emojisplosion(button) {
         span.style.position = 'fixed';
         span.style.left = `${rect.left + window.scrollX}px`;
         span.style.top = `${rect.top + window.scrollY}px`;
-        span.style.transition = 'transform 1s, opacity 1s';
+        span.style.transition = 'transform 0.5s, opacity 0.5s';
         span.style.transformOrigin = 'center';
         span.style.zIndex = 9999;
 
@@ -63,35 +63,47 @@ function emojisplosion(button) {
 
         setTimeout(() => {
             document.body.removeChild(span);
-        }, 1000);
+        }, 500);
     }
+}
+
+// Update reaction count in Firebase
+function updateReactionCount(emoji, id) {
+    const reactionRef = database.ref('reactions/' + id + '/' + emoji);
+    reactionRef.transaction((currentCount) => {
+        return (currentCount || 0) + 1;
+    });
 }
 
 // Handle button clicks
 reactionButtons.forEach(button => {
-    button.addEventListener('click', async (e) => {
+    button.addEventListener('click', (e) => {
         const emoji = e.target.dataset.emoji || e.target.parentElement.dataset.emoji;
         const container = e.target.closest('.emoji-reaction-container');
         const id = container.dataset.id;
-
-        const response = await fetch('/.netlify/functions/add-reaction', {
-            method: 'POST',
-            body: JSON.stringify({ emoji, id }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-        const data = await response.json();
-        if (data && data.count) {
-            const countSpan = button.querySelector('.emoji-count');
-            countSpan.innerText = data.count;
-        } else {
-            console.error('Unexpected response:', data);
-        }
-
+        updateReactionCount(emoji, id);
         emojisplosion(button);
     });
 });
+
+// Animate count from 0 to actual value in 3 seconds
+function animateCount(button, targetCount) {
+    let currentCount = 0;
+    const increment = targetCount / 30; // 30 frames for 3 seconds
+    const countSpan = button.querySelector('.emoji-count');
+    const interval = setInterval(() => {
+        currentCount += increment;
+        if (currentCount >= targetCount) {
+            clearInterval(interval);
+            currentCount = targetCount;
+        }
+        countSpan.innerText = Math.round(currentCount);
+    }, 100); // 100ms interval for smooth animation
+}
+
+// Load counts when the page loads
+loadCounts();
+
 
 // Load counts when the page loads
 loadCounts();
